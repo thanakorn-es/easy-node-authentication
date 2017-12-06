0105e11ef09fd3e5a6f67a40316dd37de310cc03
Thanakorn <thanakorn@extentsoft.com>
2017-12-07 02:15:37 +0700

    Removed config db

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var configDB = require('./config/database.js');
 var os = require('os');
 
 // configuration ===============================================================
